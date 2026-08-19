869f1f50b8eb2dd78bb36e19192e933b1d072093
srushtidhondge1607 <srushtidhondage@gmail.com>
2026-08-19 10:02:46 +0530

    remove invalid axios import from backend model

--- models/person.js
+++ models/person.js
@@ -1,14 +1,31 @@
-import axios from 'axios'
-const baseUrl = '/api/persons' // Use relative URL
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const mongoose = require('mongoose')
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const url = process.env.MONGODB_URI
 
-export default { getAll, create }
+console.log('connecting to', url)
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
